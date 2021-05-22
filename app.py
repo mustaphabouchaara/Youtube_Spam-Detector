5171eb73f0d037f66b508392da5ae45720aee22f
MustaphaBOUCHAARA <Mustapha.BOUCHAARA@um6p.ma>
2021-05-22 22:43:32 +0000

    first commit

--- app.py
+++ app.py
@@ -1,72 +1,44 @@
 from flask import Flask, render_template, url_for, request
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk import word_tokenize
-import pickle
+from utils import process_tweet, build_freqs, remove_punctuation, abbreviation
 
+#from sklearn.externals import joblib
 app = Flask(__name__)
-
-
+#Machine Learning code goes here
 @app.route('/')
 def home():
     return render_template('home.html')
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
     df = pd.read_csv("data/Youtube01-Psy.csv")
-    corpus = df['CONTENT']
-
-    #data cleaning
-    corpus_clean = []
-    for txt in corpus:
-        word_tokens = [word.lower() for word in word_tokenize(txt)]
-        clean_words = [word for word in word_tokens if (not word in set(stopwords.words('english')) and word.isalpha())]
-        lemmmatizer = WordNetLemmatizer()
-        clean_words = [lemmmatizer.lemmatize(word.lower()) for word in clean_words]
-        corpus_clean.append(' '.join(clean_words))
-
     df_data = df[['CONTENT', 'CLASS']]
-
     # Features and Labels
     df_x = df_data['CONTENT']
+    #df_x = remove_punctuation(df_x)
+    for i in df_x.index:
+        df_x.iloc[i]=remove_punctuation(df_x.iloc[i])
+        #df_x.iloc[i,0]=abbreviation(df_x.iloc[i,0])
     df_y = df_data.CLASS
-
-    #TFIDF
+    # Extract the features with countVectorizer
     corpus = df_x
-    cv = TfidfVectorizer(min_df=1, stop_words='english')
+    cv = CountVectorizer()
     X = cv.fit_transform(corpus)
-
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size=0.33, random_state=42)
-
-    #Logistic Regression
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import accuracy_score
-
-    LR_model = LogisticRegression(solver='liblinear')
-    LR_model.fit(X_train, y_train)
-
-    # Save Model
-    pickle.dump(LR_model, open('model.pkl', 'wb'))
-
-    # ytb_model = open('spam_model.pkl', 'rb')
-    clf = pickle.load(open('model.pkl', 'rb'))
+    X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size=0.33,random_state=42)
+    # Navie Bayes
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
     if request.method == 'POST':
         comment = request.form['comment']
+        comment+=' word'
+        comment = remove_punctuation(comment)
         data = [comment]
+        print (comment)
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
     return render_template('result.html', prediction=my_prediction)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__' :
+    app.run(host='127.0.0.1', port=5555, debug=True)
